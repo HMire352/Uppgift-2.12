4aafe4b1c392dbcc32221a08d327ec2899044828
Pingvu <alka04006@utb.vaxjo.se>
2022-09-16 14:16:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift_2._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startavgift = 300;
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            int dagar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många kilometer ska köra");
+            int kilometer = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(startavgift + dagar * 500 - 500 + kilometer);
+
+
+        }
+    }
+
+}
